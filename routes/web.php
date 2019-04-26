b7d5dfe0b1cb5ac2e732c04b9b1cb2257c8776af
Phat <phatt6898@gmail.com>
2019-04-26 11:08:34 +0700

    addifel

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+// Tạo 1 nhóm route với tiền tố customer
+Route::prefix('customer')->group(function () {
+    Route::get('index', function () {
+
+        return   view("index");
+    });
+
+    Route::get('create', function () {
+        // Hiển thị Form tạo khách hàng
+    });
+
+    Route::post('store', function () {
+        // Xử lý lưu dữ liệu tạo khách hàng thong qua phương thức POST từ form
+    });
+
+    Route::get('{id}/show', function () {
+        // Hiển thị thông tin chi tiết khách hàng có mã định danh id
+    });
+
+    Route::get('{id}/edit', function () {
+        // Hiển thị Form chỉnh sửa thông tin khách hàng
+    });
+
+    Route::patch('{id}/update', function () {
+        // xử lý lưu dữ liệu thông tin khách hàng được chỉnh sửa thông qua PATCH từ form
+    });
+
+    Route::delete('{id}', function () {
+        // Xóa thông tin dữ liệu khách hàng
+    });
+});
